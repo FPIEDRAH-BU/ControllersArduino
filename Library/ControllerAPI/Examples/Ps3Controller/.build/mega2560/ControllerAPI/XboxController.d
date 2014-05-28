54f226737b990ba124eac320013a42c78964e54b
DarkDruiD <franciscojpiedrahita@gmail.com>
2014-05-28 16:18:49 -0500

    Victor reuqests

---
+++ Library/ControllerAPI/Examples/Ps3Controller/.build/mega2560/ControllerAPI/XboxController.d
@@ -0,0 +1,33 @@
+.build/mega2560/ControllerAPI/XboxController.d .build/mega2560/ControllerAPI/XboxController.o: lib/ControllerAPI/XboxController.cpp \
+ lib/ControllerAPI/XboxController.h lib/ControllerAPI/Controller.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/mega/pins_arduino.h \
+ lib/ControllerAPI/Algorithms.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/Usb.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/settings.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/macros.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/printhex.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/message.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/hexdump.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/sink_parser.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/max3421e.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/address.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/avrpins.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/usb_ch9.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/usbhost.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/UsbCore.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/parsetools.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/confdescparser.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/controllerEnums.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/XBOXUSB.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/Usb.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/xboxEnums.h \
+ lib/USB_Host_Shield/../USB_Host_Shield/controllerEnums.h
---
+++ Library/ControllerAPI/Examples/Ps3Controller/src/sketch.ino
@@ -0,0 +1,30 @@
+#include <PS3USB.h>
+#include <usbhub.h>
+#include <PS3Controller.h>
+
+USB Usb;
+PS3USB PS3(&Usb); // This will just create the instance
+ControllerPs3 <PS3USB> ctrl(&Usb, &PS3);
+
+void setup(){
+  ctrl.setUp();
+}
+
+
+void loop()
+{
+    int16_t* hats = ctrl.getHats();
+
+    int16_t butts = ctrl.getButtons16Bit();
+    int16_t leftHatX = hats[0];
+    int16_t rightHatX = hats[1];
+    int16_t leftHatY = hats [2];
+    int16_t rightHatY = hats [3];
+
+    if(ctrl.hasChanged()){
+        Serial.println("Send Data");
+    }
+
+    delete (hats);
+}
+
